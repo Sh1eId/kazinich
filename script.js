bd717564aefdd58e4b8bde7c735c511dff09d7de
Sh1eId <116728337+Sh1eId@users.noreply.github.com>
2024-12-12 10:13:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,9 @@ document.querySelector('.spin-button').addEventListener('click', function() {
     const reels = document.querySelectorAll('.reel');
 
     reels.forEach(reel => {
-        // Генерация уникальных символов для каждой катушки
-        const uniqueSymbols = getUniqueRandomNumbers(1, 5, 3); // Генерируем 3 уникальных числа от 1 до 5
-        
+        const randomSymbol = Math.floor(Math.random() * 5); 
         reel.querySelectorAll('.symbol').forEach((symbol, index) => {
-            symbol.innerText = uniqueSymbols[index]; 
+            symbol.innerText = randomSymbol; 
         });
     });
 });
-
-// Функция для получения уникальных случайных чисел в заданном диапазоне
-function getUniqueRandomNumbers(min, max, count) {
-    const numbers = new Set();
-
-    while (numbers.size < count) {
-        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-        numbers.add(randomNum);
-    }
-
-    return Array.from(numbers);
-}
